82f3709fc3c92c19dad8eb32c141c6d8f996ed85
gabriel riquieri <gabriel2015campos@gmail.com>
2024-03-05 21:21:53 -0300

    feat: admin main page, employeItem

---
+++ scripts/admin.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    mostrarConteudo("schedual-fields")
+}
+
+function mostrarConteudo(id_conteudo) {
+    var iframes = document.querySelectorAll(".iframe-component");
+
+    iframes.forEach(elem => {
+        if (elem.id == id_conteudo) {
+            elem.classList.add("visible");
+            elem.style.height = elem.contentWindow.document.body.scrollHeight + 'px';
+        } else {
+            elem.classList.remove("visible");
+        }
+    });
+}
+
+function toggleMenu() {
+    var menu_content = document.getElementById('sanduiche');
+    menu_content.classList.toggle('expandido');
+    
+    var menu_content = document.getElementById('menu-content');
+    menu_content.classList.toggle('expandido');
+
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('expandido');
+}
